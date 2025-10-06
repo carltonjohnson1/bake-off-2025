aab2653a2510a6435b609b20a731aa13c22cb0c2
carltonjohnson1 <carltonj1@hotmail.com>
2025-10-06 14:21:14 +0100

    updated javascript to add in most contestant js

--- script.js
+++ script.js
@@ -39,3 +39,58 @@ const swiper = new Swiper('.wrapper', {
 });
 
 /* 2025 CONTESTANT SWAPPING OF IMAGE FOR TEXT*/
+
+
+// Getting Text and images for all contestants and putting in variable
+const arronInfo = document.querySelector("#aaron-info");
+const arronImage = document.querySelector("#aaron-image");
+
+const hassanInfo = document.querySelector("#hassan-info");
+const hassanImage = document.querySelector("#hassan-image");
+
+// Getting all btns and putting them in a variable
+const aaronBtn = document.querySelector("#aaron-btn");
+const aaronCloseButton = document.querySelector(".aaron-close-button");
+
+const hassanBtn = document.querySelector("#hassan-btn");
+const hassanCloseButton = document.querySelector(".hassan-close-button");
+
+
+
+// Adding event listener to buttons to switch image with text
+
+aaronBtn.addEventListener("click", function() {
+  //switching image for text on btn click
+  arronImage.style.display = "none";
+  arronInfo.style.display = "block";
+});
+
+hassanBtn.addEventListener("click", function() {
+  //switching image for text on btn click
+  hassanImage.style.display = "none";
+  hassanInfo.style.display = "block";
+});
+
+
+
+
+// Adding event listener to close button to close and switch image with text again
+
+
+aaronCloseButton.addEventListener("click", function() {
+  //switching text for image again when close button clicked
+  arronImage.style.display = "block";
+  arronInfo.style.display = "none";
+});
+
+
+hassanCloseButton.addEventListener("click", function() {
+  //switching text for image again when close button clicked
+  hassanImage.style.display = "block";
+  hassanInfo.style.display = "none";
+});
+
+
+
+
+
